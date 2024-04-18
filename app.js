fa86633d85965c9406da173ef5effe2dc838146c
perfectaeon <andrewgwozd3221@gmail.com>
2024-04-18 19:35:54 +0600

    all files

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const path = require('path')
+
+const bodyParser = require('body-parser')
+const cors = require('./middlewares/cors')
+     
+const mainRoute = require('./routes/main');
+const gamesRouter = require('./routes/games'); 
+     
+const PORT = 3005;
+const app = express();
+
+app.use(cors);
+
+app.use( bodyParser.json(), express.static(path.join(__dirname, 'public')));
+     
+app.use(mainRoute, gamesRouter, ); 
+     
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT http://localhost:${PORT}`);
+    })
+     
